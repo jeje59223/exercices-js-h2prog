5668635e3419cba63f30d46f6978a49e060dd283
jeje59223 <jcnockaert@hotmail.fr>
2020-09-26 21:26:31 +0200

    exercice 2 facile JS H2prog

---
+++ exercices-faciles/exercice2/exo2.js
@@ -0,0 +1,15 @@
+// afficher des variables
+const nom = "Toto";
+const age = 30;
+const sexe = true;
+
+console.log(`Nom : ${nom}`);
+console.log(`Age : ${age}`);
+if(sexe){
+    console.log(`Sexe : Homme`);
+} else {
+    console.log(`Sexe : Femme`);
+}
+
+
+
