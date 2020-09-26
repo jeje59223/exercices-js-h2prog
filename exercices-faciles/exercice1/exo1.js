38d67d1f58b82a0491d8eb8e3625a49fc7e9fd25
jeje59223 <jcnockaert@hotmail.fr>
2020-09-26 21:16:35 +0200

    exercice 1 facile JS H2PROG

---
+++ exercices-faciles/exercice1/exo1.js
@@ -0,0 +1,14 @@
+// inverser des valeurs
+
+let a = 1;
+let b = 5;
+let c;
+console.log("---------- Avant Inversion ----------");
+console.log(`A : ${a}`);
+console.log(`B : ${b}`);
+c = a;
+a = b;
+b = c;
+console.log("---------- Après Inversion ----------");
+console.log(`A : ${a}`);
+console.log(`B : ${b}`);
