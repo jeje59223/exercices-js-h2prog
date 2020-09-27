c5bcba2d31dd440b52621aa13bdb85353f62c9e7
jeje59223 <jcnockaert@hotmail.fr>
2020-09-27 16:04:55 +0200

    exercice 16 facile js h2prog

--- exercices-faciles/exercice16/exo16.js
+++ exercices-faciles/exercice16/exo16.js
@@ -1,2 +1,14 @@
-let nbAleatoire = Math.floor(Math.random() * Math.floor(20) + 1);
+// faire un programme qui choisit un nb aleatoire entre 0 et 20
+// si nb < 7 = petit, si nb entre 7 et 15 = moyen, nb > 15 = grand
+
+let nbAleatoire = Math.floor(Math.random() * Math.floor(20));
+
 console.log(nbAleatoire);
+
+if(nbAleatoire < 7){
+    console.log("Petit");
+} else if(nbAleatoire >= 7 && nbAleatoire <= 15){
+    console.log("Moyen");
+} else {
+    console.log("Grand");
+}
