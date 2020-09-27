a16700118bdf3bac5bc1010f580fca73c6d6c83e
jeje59223 <jcnockaert@hotmail.fr>
2020-09-27 16:28:17 +0200

    exercice 18 facile js h2prog

---
+++ exercices-faciles/exercice18/exo18.js
@@ -0,0 +1,8 @@
+// faire un programme qui additionne les 10 premiers chiffres
+
+let resultat = 0;
+
+for(let i = 1; i <= 10; i++){
+    console.log(`Etape ${i} : ${resultat = resultat + i}`);
+}
+console.log(`Le résultat des 10 premiers chiffres est égale à : ${resultat}`);
